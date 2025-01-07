1515eb53090d9173b5f7edc87bb275e1ea6fea4c
anix001 <chaudharyanish305@gmail.com>
2025-01-07 16:01:51 +0545

    Improvement: If-else clause in py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ first_name="Anish"
 
 print(first_name)
 
+# f-string example (like template string in js)
 print(f"Hey {first_name}")
+
+is_student  = True;
+
+if is_student:
+    print("Yes, I am a student")
+else:
+    print("No, I am not a  student")
