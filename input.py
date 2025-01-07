7bbb62a459d3cbb6d62726837f66ebc34d623fe5
anix001 <chaudharyanish305@gmail.com>
2025-01-07 17:42:39 +0545

    Improvement: input() in py

---
+++ input.py
@@ -0,0 +1,17 @@
+# input() = A function that prompts user to enter data. Returns the enter data as string.
+
+name = input("What is your name?: ")
+print(f"Hello {name} !!")
+
+
+# Excercise 1: Calculate the area of rectangle
+
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+area = length * width
+
+print(f"The area of rectangle is {area}")
+
+
+
