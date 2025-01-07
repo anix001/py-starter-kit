51a9fa715b0415e847d51147e5bf3d17f9c21351
anix001 <chaudharyanish305@gmail.com>
2025-01-07 17:21:48 +0545

    Improvement: Type Casting

---
+++ typeCasting.py
@@ -0,0 +1,15 @@
+# Typecasting = The process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+
+
+name= "Arpana Chaudhary"
+age= 18
+gpa=3.0
+is_Student =True
+
+print(type(name)) # Checking the type
+
+# Converting from float to int
+gpa = int(gpa)
+print(gpa) #3
