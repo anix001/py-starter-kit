0dcdd0be30d0a7232a68db276aa6c087adddac2d
anix001 <chaudharyanish305@gmail.com>
2025-01-08 17:10:40 +0545

    Improvement: Artithmetic operation and math module

---
+++ math.py
@@ -0,0 +1,19 @@
+# arithmetic operators
+ # type: ignore
+
+friends = 5
+
+# friends = friends + 1
+# friends += 1
+# friends -=2
+# friends *=2
+# friends /=2
+# friends **=2
+#print(friends)
+
+
+# round(3.12), pow(5,2), abs(-3), max(1, 2, 3), min(1,2,3)
+
+import math
+
+print(math.pi)
