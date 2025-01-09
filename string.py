4e62f6525329528420876225d7bd94139653c13a
anix001 <chaudharyanish305@gmail.com>
2025-01-09 14:49:01 +0545

    Improvement: Indexing in string

--- string.py
+++ string.py
@@ -4,12 +4,26 @@
 # should not contain any digits
 
 
-username = input("Enter username: ")
+# username = input("Enter username: ")
 
-if len(username)>12 or not username.isalpha():
-    print("Invalid user name")
-else:
-    print("Valid username")
+# if len(username)>12 or not username.isalpha():
+#     print("Invalid user name")
+# else:
+#     print("Valid username")
 
 # print(username.isalpha())
-# print(len(username))b
+# print(len(username))
+
+
+# Indexing - accessing  elements of a sequence using [] (indexing operator)
+#  [start: end :step]
+
+credit_card_number = "1234-5675-9874-5555"
+
+print(credit_card_number[5])
+print(credit_card_number[:5])
+print(credit_card_number[5:])
+print(credit_card_number[::3]) #count every third character from 0
+
+#reverse string
+print(credit_card_number[::-1])
