94edd22be4af9137b080ddcee603bfd33723f098
anix001 <chaudharyanish305@gmail.com>
2025-01-10 17:49:05 +0545

    Improvement: Functions

---
+++ functionInPython.py
@@ -0,0 +1,11 @@
+# function = a block of reusable code
+# place () after function name to revoke it
+
+def happy_birthday():
+    print("Happy birthday arpu!!")
+
+
+happy_birthday()
+
+
+# Default arguments = a default value for certain parameters. default is used when the argument is omitted
