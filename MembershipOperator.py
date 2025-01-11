8e7cce9f96eb528b5d6c8d828bb554365ea95f33
anix001 <chaudharyanish305@gmail.com>
2025-01-11 15:52:45 +0545

    Improvement: Memebership Operator

---
+++ MembershipOperator.py
@@ -0,0 +1,15 @@
+# Membership Operator = used to test a value  or variable is found in a sequence(string,list, set, tuple, dictionary)
+# 1. in      2. not in
+
+
+
+word = "apple"
+
+letter = input("guess a letter in the secret word:")
+
+if letter not in word:
+    print("Oh, There is no such letter in word")
+
+
+if letter in word:
+    print(f"{letter} is in word")
