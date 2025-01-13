85537a6d2de9f28a47eba8cfd8a1520306251e39
anix001 <chaudharyanish305@gmail.com>
2025-01-13 17:08:26 +0545

    Improvement: Property decerator

---
+++ PropertyDecorator.py
@@ -0,0 +1,55 @@
+# @Property = Decorator used to define a method as a property(it can be accessed like an attribute)
+#             Benefit: Add additional logic when read, write, or delete attributes
+#             Gives you getter, setter, and delete method
+
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height # IF WE ADD _  BEFORE THE ATTRIBUTE IT WILL MAKE IT PROTECTED
+
+    @property
+    def width(self):
+        return f"{self._width:1f}cm"
+    
+    @property
+    def height(self):
+        return f"{self._height:1f}cm"
+    
+    @width.setter
+    def width(self, new_width):
+        if new_width >0 :
+            self._width = new_width
+        else:
+            print("Width  must be greater than 0")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height >0 :
+            self._height = new_height
+        else:
+            print("Height must be greater than 0")
+    
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width has been deleted")
+
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height has been deleted")
+    
+rectangle = Rectangle(3,4)
+
+
+rectangle.height = 15
+rectangle.width = 25
+
+# to delete
+del rectangle.width
+del rectangle.height
+
+# print(rectangle.width)
+# print(rectangle.height)
