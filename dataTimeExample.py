031849041e6689fd6ec1182aeab4a72369df0dc2
anix001 <chaudharyanish305@gmail.com>
2025-01-13 22:03:16 +0545

    Improvement: Datetime module

---
+++ dataTimeExample.py
@@ -0,0 +1,12 @@
+import datetime
+
+date = datetime.date(2025, 1,2)
+today = datetime.date.today()
+
+
+time = datetime.time(12,30, 0)
+now = datetime.datetime.now()
+
+now = now.strftime("%H:%M:%S %m %d %y")
+
+print(now)
